1a53021f16b6ec1865a7e7144ea77b644f078d44
PedroEtche <pedro.etchegoyen@gmail.com>
2022-11-09 20:59:05 -0300

    Se Agregago Codigo Suficiente Para El Caso 10

--- src/main/test/edu/fiuba/algo3/casoDeUso10/CasoDeUso10.java
+++ src/main/test/edu/fiuba/algo3/casoDeUso10/CasoDeUso10.java
@@ -2,6 +2,7 @@ package edu.fiuba.algo3.casoDeUso10;
 
 import edu.fiuba.algo3.modelo.*;
 import edu.fiuba.algo3.modelo.Tablero.Moho;
+import edu.fiuba.algo3.modelo.Tablero.Neutro;
 import edu.fiuba.algo3.modelo.Tablero.SinRecurso;
 import edu.fiuba.algo3.modelo.Tablero.VolcanGasVespeno;
 import org.junit.jupiter.api.Test;
@@ -90,23 +91,110 @@ public class CasoDeUso10 {
     @Test
     public void seConstruyeUnExtractorSeLoDaniaYSuVidaEsLaEsperada(){
         Imperio zergs = new Zergs(new Recurso(), new Recurso());
-        NodoCompatible nodoCompatibleReserva = new NodoCompatible(new Moho(), new VolcanGasVespeno());
-        Extractor extractor = new Extractor(nodoCompatibleReserva);
+        NodoCompatible nodoCompatibleReserva = new NodoCompatible(new Neutro(), new VolcanGasVespeno());
+        Extractor extractor = new Extractor(nodoCompatibleReserva, new Recurso(100));
+
+        // Paso 12 turnos para que se construya la reservaDeReproduccion
+        for (int i = 0; i < 6; i++){
+            extractor.accionDeTurno();
+        }
+
+        extractor.recibirDanio(500);
+        int vidaEsperada = 250;
+
+        assertEquals(vidaEsperada, extractor.getVida());
+    }
+
+    @Test
+    public void seConstruyeUnExtractorSeLoDaniaYPasaTurnosHastaQueEsteCurado(){
+        Imperio zergs = new Zergs(new Recurso(), new Recurso());
+        NodoCompatible nodoCompatibleReserva = new NodoCompatible(new Neutro(), new VolcanGasVespeno());
+        Extractor extractor = new Extractor(nodoCompatibleReserva, new Recurso(100));
+
+        // Paso 6 turnos para que se construya la reservaDeReproduccion
+        for (int i = 0; i < 6; i++){
+            extractor.accionDeTurno();
+        }
+
+        extractor.recibirDanio(500);
+
+        extractor.accionDeTurno();
+        extractor.accionDeTurno();
+        extractor.accionDeTurno();
+
+        int vidaEsperada = 750;
+
+        assertEquals(vidaEsperada, extractor.getVida());
+    }
+
+    @Test
+    public void seConstruyeUnaGuaridaSeLoDaniaYSuVidaEsLaEsperada(){
+        Imperio zergs = new Zergs(new Recurso(), new Recurso());
+        NodoCompatible nodoCompatibleGuarida= new NodoCompatible(new Moho(), new SinRecurso());
+        Guarida guarida = new Guarida(nodoCompatibleGuarida);
 
         // Paso 12 turnos para que se construya la reservaDeReproduccion
         for (int i = 0; i < 12; i++){
-            reservaDeReproduccion.accionDeTurno();
+            guarida.accionDeTurno();
         }
 
-        reservaDeReproduccion.recibirDanio(500);
-
-        reservaDeReproduccion.accionDeTurno();
-        reservaDeReproduccion.accionDeTurno();
-
+        guarida.recibirDanio(250);
         int vidaEsperada = 1000;
 
-        assertEquals(vidaEsperada, reservaDeReproduccion.getVida());
+        assertEquals(vidaEsperada, guarida.getVida());
     }
 
+    @Test
+    public void seConstruyeUnaGuaridaSeLoDaniaYDespuesDeUnosTurnosSeRecuperaTodaLaVida(){
+        Imperio zergs = new Zergs(new Recurso(), new Recurso());
+        NodoCompatible nodoCompatibleGuarida= new NodoCompatible(new Moho(), new SinRecurso());
+        Guarida guarida = new Guarida(nodoCompatibleGuarida);
 
+        // Paso 12 turnos para que se construya la reservaDeReproduccion
+        for (int i = 0; i < 12; i++){
+            guarida.accionDeTurno();
+        }
+
+        guarida.recibirDanio(250);
+        guarida.accionDeTurno();
+        int vidaEsperada = 1250;
+
+        assertEquals(vidaEsperada, guarida.getVida());
+    }
+
+    @Test
+    public void seConstruyeUnaEspiralSeLoDaniaYSuVidaEsLaEsperada(){
+        Imperio zergs = new Zergs(new Recurso(), new Recurso());
+        NodoCompatible nodoCompatibleGuarida= new NodoCompatible(new Moho(), new SinRecurso());
+        Espiral espiral = new Espiral(nodoCompatibleGuarida);
+
+        // Paso 10 turnos para que se construya la reservaDeReproduccion
+        for (int i = 0; i < 10; i++){
+            espiral.accionDeTurno();
+        }
+
+        espiral.recibirDanio(350);
+        int vidaEsperada = 1000;
+
+        assertEquals(vidaEsperada, espiral.getVida());
+    }
+
+    @Test
+    public void seConstruyeUnaEspiralSeLoDaniaYSePasanVariosTurnosYSeRecuperaTodaLaVida(){
+        Imperio zergs = new Zergs(new Recurso(), new Recurso());
+        NodoCompatible nodoCompatibleGuarida= new NodoCompatible(new Moho(), new SinRecurso());
+        Espiral espiral = new Espiral(nodoCompatibleGuarida);
+
+        // Paso 10 turnos para que se construya la reservaDeReproduccion
+        for (int i = 0; i < 10; i++){
+            espiral.accionDeTurno();
+        }
+
+        espiral.recibirDanio(350);
+        espiral.accionDeTurno();
+        espiral.accionDeTurno();
+        int vidaEsperada = 1350;
+
+        assertEquals(vidaEsperada, espiral.getVida());
+    }
 }
