c5864af94ad8d007402aacdad323f0106e12d066
ezebenza2000 <ebenzaquen@fi.uba.ar>
2022-11-09 21:08:27 -0300

    modificarRecurso en NodoRecurso Implementado

--- src/main/java/edu/fiuba/algo3/modelo/Tablero/NodoRecurso.java
+++ src/main/java/edu/fiuba/algo3/modelo/Tablero/NodoRecurso.java
@@ -1,5 +1,10 @@
 package edu.fiuba.algo3.modelo.Tablero;
 
+import edu.fiuba.algo3.modelo.Recurso;
+
 public interface NodoRecurso {
     boolean igualA(NodoRecurso nodoRecurso);
+
+    void modificarRecurso(Recurso recurso , int cantidadExtraccion);
+
 }
