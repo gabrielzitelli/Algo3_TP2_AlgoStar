3ae36e00e21b52714276026e95186b00f4706ef1
ezebenza2000 <ebenzaquen@fi.uba.ar>
2022-11-09 21:14:45 -0300

    faltaba un metodo definido de la abstracta (esta mal diseñado)

--- src/main/java/edu/fiuba/algo3/modelo/Tablero/SinRecurso.java
+++ src/main/java/edu/fiuba/algo3/modelo/Tablero/SinRecurso.java
@@ -1,9 +1,13 @@
 package edu.fiuba.algo3.modelo.Tablero;
 
+import edu.fiuba.algo3.modelo.Recurso;
+
 public class SinRecurso implements NodoRecurso{
     @Override
     public boolean igualA(NodoRecurso nodoRecurso) {
         SinRecurso sinRecurso = new SinRecurso();
         return sinRecurso.getClass().equals(nodoRecurso.getClass());
+    }
+    public void modificarRecurso(Recurso _minerales , int cantidadExtraccion){
     }
 }
