c1d39f6314ab86f150819ca3f24118b304047290
gabrielzitelli <gzitelli@fi.uba.ar>
2022-11-10 06:24:07 -0300

    Arreglo y agregado de pruebas del caso de uso 6

--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso6Test.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso6Test.java
@@ -4,28 +4,68 @@ import edu.fiuba.algo3.modelo.*;
 import edu.fiuba.algo3.modelo.Tablero.Neutro;
 import edu.fiuba.algo3.modelo.Tablero.SinRecurso;
 import edu.fiuba.algo3.modelo.Tablero.Tablero;
+import edu.fiuba.algo3.modelo.excepciones.TerrenoNoCompatibleConEdificio;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CasoDeUso6Test {
 
     @Test
-    public void MohoAumentaEn1DespuesDe2TurnosDelCriaderoActivo() {
+    public void MohoAumentaEn1DespuesDe2TurnosDelCriaderoEstandoActivo() {
         Tablero tablero = new Tablero(10, 10);
-        Zergs zergs = new Zergs(tablero, new Recurso(50), new Recurso());
+        Zergs zergs = new Zergs(tablero, new Recurso(150), new Recurso());
         Zangano zangano = new Zangano(tablero, new Coordenadas(0,0), new Recurso());
 
         NodoCompatible nodoCompatible = new NodoCompatible(new Neutro(), new SinRecurso());
         Criadero criadero = new Criadero(tablero, nodoCompatible, new Coordenadas(0,0), zergs);
         tablero.construir(criadero, zangano.getPosicion());
 
-        Zangano zanganoParaCriadero = new Zangano(tablero, new Coordenadas(5,0), new Recurso());
+        Zangano zanganoParaReserva = new Zangano(tablero, new Coordenadas(5,0), new Recurso());
 
         criadero.accionDeTurno();
         criadero.accionDeTurno();
 
+        assertDoesNotThrow(() -> zergs.construirReservaDeReproduccion(zanganoParaReserva));
+    }
 
-        assertDoesNotThrow(() -> zergs.construirCriadero(zanganoParaCriadero, new Coordenadas(0,0)));
+    @Test
+    public void MohoNoAumentaEn1DespuesDe1TurnoDelCriaderoEstandoActivo() {
+        Tablero tablero = new Tablero(10, 10);
+        Zergs zergs = new Zergs(tablero, new Recurso(150), new Recurso());
+        Zangano zangano = new Zangano(tablero, new Coordenadas(0,0), new Recurso());
+
+        NodoCompatible nodoCompatible = new NodoCompatible(new Neutro(), new SinRecurso());
+        Criadero criadero = new Criadero(tablero, nodoCompatible, new Coordenadas(0,0), zergs);
+        tablero.construir(criadero, zangano.getPosicion());
+
+        Zangano zanganoParaReserva = new Zangano(tablero, new Coordenadas(5,0), new Recurso());
+
+        criadero.accionDeTurno();
+
+        assertThrows(TerrenoNoCompatibleConEdificio.class, () -> {
+            zergs.construirReservaDeReproduccion(zanganoParaReserva);
+        });
+    }
+
+    @Test
+    public void IntentarConstruirEnRango2SiMohoSoloAumentoEn1LanzaExcepcion() {
+        Tablero tablero = new Tablero(10, 10);
+        Zergs zergs = new Zergs(tablero, new Recurso(150), new Recurso());
+        Zangano zangano = new Zangano(tablero, new Coordenadas(0,0), new Recurso());
+
+        NodoCompatible nodoCompatible = new NodoCompatible(new Neutro(), new SinRecurso());
+        Criadero criadero = new Criadero(tablero, nodoCompatible, new Coordenadas(0,0), zergs);
+        tablero.construir(criadero, zangano.getPosicion());
+
+        Zangano zanganoParaReserva = new Zangano(tablero, new Coordenadas(6,0), new Recurso());
+
+        criadero.accionDeTurno();
+        criadero.accionDeTurno();
+
+        assertThrows(TerrenoNoCompatibleConEdificio.class, () -> {
+            zergs.construirReservaDeReproduccion(zanganoParaReserva);
+        });
     }
 }
