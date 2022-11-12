255bfbf6898e43e62d539b6436b5fcb78c6be4aa
PedroEtche <pedro.etchegoyen@gmail.com>
2022-11-12 18:51:09 -0300

    Implemento Guarida con patron state

---
+++ src/main/test/edu/fiuba/algo3/GuaridaTest.java
@@ -0,0 +1,34 @@
+package edu.fiuba.algo3;
+
+import edu.fiuba.algo3.modelo.Edificio_Zerg.Guarida;
+import edu.fiuba.algo3.modelo.Edificio_Zerg.ReservaDeProduccion;
+import edu.fiuba.algo3.modelo.Excepciones.ErrorEdificioNoEstaConstruido;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class GuaridaTest {
+
+    @Test
+    public void test01UnaGuaridaNoSeConstruyeEn11Turnos(){
+        Guarida unaGuarida = new Guarida();
+
+        for(int i = 0; i < 11; i++)
+            unaGuarida.pasarTurno();
+
+
+        assertThrows(ErrorEdificioNoEstaConstruido.class, () -> unaGuarida.crearFabricaHidralisco());
+    }
+
+    @Test
+    public void test02GuaridaSePuedeConstruirEn12Turnos(){
+        Guarida unaGuarida = new Guarida();
+
+        for(int i = 0; i < 12; i++)
+            unaGuarida.pasarTurno();
+
+        assertDoesNotThrow(() -> unaGuarida.crearFabricaHidralisco());
+    }
+
+}
