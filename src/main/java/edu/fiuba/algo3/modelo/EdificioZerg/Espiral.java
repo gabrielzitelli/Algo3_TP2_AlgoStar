b19f8345a931209a57e703efd06752147330b142
ezebenza2000 <ebenzaquen@fi.uba.ar>
2022-11-13 17:49:44 -0300

    Espiral y Guarida se construyen en mapa

--- src/main/java/edu/fiuba/algo3/modelo/EdificioZerg/Espiral.java
+++ src/main/java/edu/fiuba/algo3/modelo/EdificioZerg/Espiral.java
@@ -1,13 +1,19 @@
 package edu.fiuba.algo3.modelo.EdificioZerg;
 
+import edu.fiuba.algo3.modelo.Edificio;
+import edu.fiuba.algo3.modelo.Mapa.Casilla.*;
 import edu.fiuba.algo3.modelo.States.EstadoEspiral;
 import edu.fiuba.algo3.modelo.States.EstadoEspiralEnConstruccion;
 
-public class Espiral {
+public class Espiral implements Edificio {
 
     private EstadoEspiral estado;
 
     private int turnoParaEstarConstruido = 10;
+
+    protected Recolectable estadoRecolectable = new NoRecolectable();
+    protected Cargable estadoCarga = new SinCarga();
+    protected EstadoMoho estadoMoho = new ConMoho();
 
     public Espiral() {
         estado = new EstadoEspiralEnConstruccion(turnoParaEstarConstruido);
@@ -19,5 +25,11 @@ public class Espiral {
 
     public FabricaMutalisco crearFabricaMutalisco() {
         return estado.crearFabricaMutalisco();
+    }
+
+    public void verificarConstruccion(Casilla unaCasilla){
+        unaCasilla.tieneEsteRecoletable(estadoRecolectable);
+        unaCasilla.tieneEstaCarga(estadoCarga);
+        unaCasilla.tieneEsteMoho(estadoMoho);
     }
 }
--- src/main/java/edu/fiuba/algo3/modelo/EdificioZerg/Guarida.java
+++ src/main/java/edu/fiuba/algo3/modelo/EdificioZerg/Guarida.java
@@ -1,13 +1,20 @@
 package edu.fiuba.algo3.modelo.EdificioZerg;
 
+import edu.fiuba.algo3.modelo.Edificio;
+import edu.fiuba.algo3.modelo.Mapa.Casilla.*;
 import edu.fiuba.algo3.modelo.States.EstadoGuarida;
 import edu.fiuba.algo3.modelo.States.EstadoGuaridaEnConstruccion;
 
-public class Guarida {
+public class Guarida implements Edificio {
 
     private EstadoGuarida estado;
 
     private int turnoParaEstarConstruido = 12;
+
+
+    protected Recolectable estadoRecolectable = new NoRecolectable();
+    protected Cargable estadoCarga = new SinCarga();
+    protected EstadoMoho estadoMoho = new ConMoho();
 
     public Guarida(){
         //Aplicacion de patron State
@@ -20,5 +27,11 @@ public class Guarida {
 
     public FabricaHidralisco crearFabricaHidralisco() {
         return estado.crearFabricaHidralisco();
+    }
+
+    public void verificarConstruccion(Casilla unaCasilla){
+        unaCasilla.tieneEsteRecoletable(estadoRecolectable);
+        unaCasilla.tieneEstaCarga(estadoCarga);
+        unaCasilla.tieneEsteMoho(estadoMoho);
     }
 }
