cc91e5faba56e32c8e712e8b6a10f6f62a4fe317
PedroEtche <pedro.etchegoyen@gmail.com>
2022-11-28 17:23:59 -0300

    Agrego pantalla de seleccion de jugador

--- src/main/java/edu/fiuba/algo3/controladores/BienvenidaControlador.java
+++ src/main/java/edu/fiuba/algo3/controladores/BienvenidaControlador.java
@@ -5,12 +5,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -51,7 +48,7 @@ public class BienvenidaControlador extends Controlador implements Initializable 
 
     @FXML
     public void empezarCreacionJugador(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/vistas/datosJugadoresVista.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/vistas/datosJugador1Vista.fxml"));
         root = loader.load();
 
         stage = obtenerStageActual(event);
